79a258d2b6a8423581d957e709f69040353ef6d7
Amine RACHYD <aminerachyd99@gmail.com>
2022-07-12 09:19:29 +0200

    Updated rhacm policies
    Signed-off-by: Amine RACHYD <aminerachyd99@gmail.com>

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ spec:
           serviceAccountName: configure-users-sa
           containers:
             - name: set-permissions
-              image: quay.io/aminerachyd/ro-users-k8s:0.0.4
+              image: quay.io/aminerachyd/ro-users-k8s:0.0.5
               command:
                 - /bin/sh
                 - -c
